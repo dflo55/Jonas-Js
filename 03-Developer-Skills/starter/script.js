88ab53e2f79e0171ebf9f35f9c414b278fc57a03
dflo55 <dflores2013@yahoo.com>
2022-06-08 04:52:40 -0500

    started lessons again, lets learn!

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,9 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = 25;
+
+const calcAge = birthYear => 2022 - birthYear;
+console.log(calcAge(1997));
+
+const ebayCalculator = () => {};
