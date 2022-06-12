f3ef44fa14db2a5b7c36d768c89d4120aa1efde4
dflo55 <dflores2013@yahoo.com>
2022-06-11 21:29:11 -0500

    updating developer file as well as practiced js file

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,9 +1,95 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = 25;
+// Problem 1: Calculate temperature amplitude
+// const temperature = [3, -2, -6, -1, `error`, 9, 13, 17, 15, 14, 9, 5];
+// 1. understanding the problem
+// what is a temp amplitude? Answer: difference between highest and lowest temp.
+// how to compute max and min temperatures?
+// what is a sensor error? what do we do?
 
-const calcAge = birthYear => 2022 - birthYear;
-console.log(calcAge(1997));
+// 2. breaking up into sub problems
+// how to ignore errors
+// find max value in temp array
+// find min value in temp array
+// subtract min from max (amplitude) and return it
+// const calcTempAmplitude = function (t1, t2) {
+//   const temps = t1.concat(t2);
+//   console.log(temps);
 
-const ebayCalculator = () => {};
+//   let max = temps[0];
+//   let min = temps[0];
+//   for (let i = 0; i < temperature.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== `number`) continue;
+//     if (curTemp > max) {
+//       max = curTemp;
+//     }
+//     if (curTemp < min) {
+//       min = curTemp;
+//     }
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+
+// const amplitude = calcTempAmplitude([3, 5, 1], [9, 0, 5]);
+// console.log(amplitude);
+
+// Problem 2:
+// Function should now receive 2 arrays of temperatures
+
+// 1) understanding the problem
+// - with 2 arrays, should we implement functionality twice?
+//  NO! just merge the two arrays
+
+// 2) breaking into sub problems
+// how to merge 2 arrays?
+
+// Debugging
+const measureKelvin = function () {
+  const measurement = {
+    type: `temp`,
+    unit: `celsius`,
+    value: 10,
+  };
+  // C. Fix the big
+  //     value: Number(prompt(`Degress celsius:`)),
+
+  // B. Find the Bug
+  console.log(measurement);
+  console.table(measurement);
+  //   console.log(measurement.value);
+  //   console.warn(measurement.value);
+  //   console.error(measurement.value);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A. Identify the Bug
+//
+console.log(measureKelvin());
+
+// Using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== `number`) continue;
+    if (curTemp > max) {
+      max = curTemp;
+    }
+    if (curTemp < min) {
+      min = curTemp;
+    }
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// A. Identify bug
+console.log(amplitudeBug);
--- jonas-practice/script.js
+++ jonas-practice/script.js
@@ -697,24 +697,24 @@
 // Let's improve Steven's tip calculator even more, this time using loops!
 // Your tasks:
 // 1. Create an array 'bills' containing all 10 test bill values
-const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 // 2. Create empty arrays for the tips and the totals ('tips' and 'totals')
-const tips = [];
-const totals = [];
+// const tips = [];
+// const totals = [];
 // 3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate
 // tips and total values (bill + tip) for every bill value in the bills array. Use a for
 // loop to perform the 10 calculations!
-const calcTip = function (bill) {
-  const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-  return tip;
-};
+// const calcTip = function (bill) {
+//   const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+//   return tip;
+// };
 
-for (let i = 0; i < bills.length; i++) {
-  tips.push(calcTip(bills[i]));
-  totals.push(tips[i] + bills[i]);
-}
-console.log(tips);
-console.log(totals);
+// for (let i = 0; i < bills.length; i++) {
+//   tips.push(calcTip(bills[i]));
+//   totals.push(tips[i] + bills[i]);
+// }
+// console.log(tips);
+// console.log(totals);
 // Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
 // Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
 // tips and totals arrays �
@@ -723,16 +723,16 @@ console.log(totals);
 // 4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as
 // an argument. This function calculates the average of all numbers in the given
 // array. This is a difficult challenge (we haven't done this before)! Here is how to
-// solve it:
-const calcAverage = function (arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
-  }
-  return sum / arr.length;
-};
-const averageTotal = calcAverage(totals);
-console.log(averageTotal);
+//  solve it:
+// const calcAverage = function (arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   return sum / arr.length;
+// };
+// const averageTotal = calcAverage(totals);
+// console.log(averageTotal);
 // 4.1. First, you will need to add up all values in the array. To do the addition,
 // start by creating a variable 'sum' that starts at 0. Then loop over the
 // array using a for loop. In each iteration, add the current value to the
@@ -741,3 +741,50 @@ console.log(averageTotal);
 // 4.2. To calculate the average, divide the sum you calculated before by the
 // length of the array (because that's the number of elements)
 // 4.3. Call the function with the 'totals' array
+
+////////////   Developer Skills & Editor Setup  \\\\\\\\\\\\
+
+////*Coding Challenge #1*////
+// Given an array of forecasted maximum temperatures, the thermometer displays a
+// string with the given temperatures. Example: [17, 21, 23] will print "... 17ºC in 1
+// days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+// Your tasks:
+// 1. Create a function 'printForecast' which takes in an array 'arr' and logs a
+// string like the above to the console. Try it with both test datasets.
+
+// 2. Use the problem-solving framework: Understand the problem and break it up
+// into sub-problems!
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+// Test data:
+// § Data 1: [17, 21, 23]
+// § Data 2: [12, 5, -5, 0, 4]
+
+// deliver thermometer message stating each max temp in accordance to order they are in
+// create a function
+// arrange numbers in increasing order
+// 1 paramater of arr
+// want it to log a string of temp in x days
+
+// const printForecast = function (arr) {
+//   let days = 1;
+//   arr.sort(function (a, b) {
+//     return a - b;
+//   });
+//   for (let i = 0; i < arr.length; i++) {
+//     console.log(`${arr[i]}c in ${days++} days`);
+//   }
+// };
+
+// printForecast(data1);
+// printForecast(data2);
+
+const printForecast = function (arr) {
+  let string = ``;
+  for (let i = 0; i < arr.length; i++) {
+    string = string + `${arr[i]}c in ${i + 1} days ... `;
+  }
+  console.log(`...` + string);
+};
+
+printForecast(data1);
