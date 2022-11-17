79cd19e02e0d810e2447e46487a9c5fbc04e2598
dflo55 <dflores2013@yahoo.com>
2022-11-17 04:35:44 -0600

    Finished Functions section

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -276,86 +276,197 @@
 // Hints: Use many of the tools you learned about in this and the last section �
 // GOOD LUCK
 
-const poll = {
-  question: "What is your favourite programming language?",
-  options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
-  // This generates [0, 0, 0, 0]. More in the next section!
-  answers: new Array(4).fill(0),
+// const poll = {
+//   question: "What is your favourite programming language?",
+//   options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+//   // This generates [0, 0, 0, 0]. More in the next section!
+//   answers: new Array(4).fill(0),
 
-  registerNewAnswer() {
-    console.log(this);
-    // const answer = Number(prompt(
-    //   `${this.question}\m${this.options.join(`\n`)}\n(Write option number)`
-    // ));
-    const answer = prompt(
-      `${this.question}\m${this.options.join(`\n`)}\n(Write option number)`
-    );
-    const num = parseInt(answer);
+//   registerNewAnswer() {
+//     console.log(this);
+//     // const answer = Number(prompt(
+//     //   `${this.question}\m${this.options.join(`\n`)}\n(Write option number)`
+//     // ));
+//     const answer = prompt(
+//       `${this.question}\m${this.options.join(`\n`)}\n(Write option number)`
+//     );
+//     const num = parseInt(answer);
 
-    // Register Answer
-    // typeof answer === `number` &&
-    //   answer < this.answers.length &&
-    //   this.answers[answer]++;
+//     // Register Answer
+//     // typeof answer === `number` &&
+//     //   answer < this.answers.length &&
+//     //   this.answers[answer]++;
 
-    // Another if statement way
-    // if (num === 0) {
-    //   this.answers[0]++;
-    // } else if (num === 1) {
-    //   this.answers[1]++;
-    // } else if (num === 2) {
-    //   this.answers[2]++;
-    // } else if (num === 3) {
-    //   this.answers[3]++;
-    // } else {
-    //   alert(`Sorry this not a valid answer.`);
-    // }
-    switch (num) {
-      case 0:
-        this.answers[0]++;
-        break;
-      case 1:
-        this.answers[1]++;
-        break;
-      case 2:
-        this.answers[2]++;
-        break;
-      case 3:
-        this.answers[3]++;
-        break;
-      default:
-        alert(`Sorry this is not a valid answer.`);
-    }
-    return this.displayResults();
-    //display prompt window for user to input the number of selected option
-    // based on input number, update the answers array property
-  },
+//     // Another if statement way
+//     // if (num === 0) {
+//     //   this.answers[0]++;
+//     // } else if (num === 1) {
+//     //   this.answers[1]++;
+//     // } else if (num === 2) {
+//     //   this.answers[2]++;
+//     // } else if (num === 3) {
+//     //   this.answers[3]++;
+//     // } else {
+//     //   alert(`Sorry this not a valid answer.`);
+//     // }
+//     switch (num) {
+//       case 0:
+//         this.answers[0]++;
+//         break;
+//       case 1:
+//         this.answers[1]++;
+//         break;
+//       case 2:
+//         this.answers[2]++;
+//         break;
+//       case 3:
+//         this.answers[3]++;
+//         break;
+//       default:
+//         alert(`Sorry this is not a valid answer.`);
+//     }
+//     return this.displayResults();
+//     //display prompt window for user to input the number of selected option
+//     // based on input number, update the answers array property
+//   },
 
-  displayResults(type = `array`) {
-    if (type === `array`) {
-      console.log(this.answers);
-    } else if (type === `string`) {
-      console.log(`Poll results are ${this.answers.join(`, `)}`);
-    }
-  },
-  // displayResults() {
-  //   const question = prompt(`Is this a string or an array?`);
-  //   if (question === `array`) {
-  //     console.log(this.answers);
-  //   } else if (question === `string`) {
-  //     console.log(
-  //       `Poll results are ${this.answers[0]}, ${this.answers[1]}, ${this.answers[2]}, ${this.answers[3]}.`
-  //     );
-  //   } else {
-  //     console.log(`FIX IT`);
-  //   }
-  // },
-};
+//   displayResults(type = `array`) {
+//     if (type === `array`) {
+//       console.log(this.answers);
+//     } else if (type === `string`) {
+//       console.log(`Poll results are ${this.answers.join(`, `)}`);
+//     }
+//   },
+// displayResults() {
+//   const question = prompt(`Is this a string or an array?`);
+//   if (question === `array`) {
+//     console.log(this.answers);
+//   } else if (question === `string`) {
+//     console.log(
+//       `Poll results are ${this.answers[0]}, ${this.answers[1]}, ${this.answers[2]}, ${this.answers[3]}.`
+//     );
+//   } else {
+//     console.log(`FIX IT`);
+//   }
+// },
+// };
 
-document
-  .querySelector(`.poll`)
-  .addEventListener(`click`, poll.registerNewAnswer.bind(poll));
+// document
+//   .querySelector(`.poll`)
+//   .addEventListener(`click`, poll.registerNewAnswer.bind(poll));
 
-poll.displayResults.call({ answers: [5, 2, 3] }, `string`);
+//   // BONUS
+// poll.displayResults.call({ answers: [5, 2, 3] }, `string`);
 
-// [5, 2, 3]
-// [1, 5, 3, 9, 6, 1];
+// // [5, 2, 3]
+// // [1, 5, 3, 9, 6, 1];
+
+//////// 136. Immediately Invoked Function Expressions (IIFE) ////////
+// const runOnce = function () {
+//   console.log(`this will never run again`);
+// };
+// runOnce();
+
+// (function () {
+//   console.log(`this will never run again`);
+//   // const isPrivate = 23;
+// })();
+
+// (() => {
+//   console.log(`this will never run again`);
+// })();
+
+/////// 137. Closures  ////////
+// const secureBooking = function () {
+//   let passengerCount = 0;
+
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
+
+// const booker = secureBooking();
+
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
+/* - Closures happen in certain situations on their own
+- Closure makes a function remember all the variables that existed at the functions birthplace
+- A function has access to the variable environment of the execution context in which it was created even 
+though it is gone. Closure: Variable environment attached to the function, exactly as it was at the time 
+and place the function was created*/
+// A closure is the closed-over variable environment of the execution context in which a function was created,
+// even after that execution context is gone.
+// A closure gives a function access to all the variables of its own parent function, even after that parent function
+// has returned. The function keeps a reference to its outer scope, which preserves the scope chain throughout time.
+// A closure makes sure that function doesn't loose connection variables that existed at the function's birth place
+// A closure is like a backpack that a function carries around wherever it goes. This backpack has all the variables
+// that were present in the environment where the function was created.
+
+//////// 138. More Closure Examples ////////
+// We dont need to return a function from another function to create a closure
+// Example 1
+// let f;
+
+// const g = function () {
+//   const a = 23;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
+
+// const h = function () {
+//   const b = 777;
+//   f = function () {
+//     console.log(b * 2);
+//   };
+// };
+
+// g();
+// f(); // different from bottom f function
+// console.dir(f);
+
+// h();
+// f(); // different from top f function
+// console.dir(f);
+
+// Example 2
+// const boardPassenger = function (n, wait) {
+//   const perGroup = n / 3;
+
+//   setTimeout(function () {
+//     console.log(`We are now boarding all ${n} passengers`);
+//     console.log(`There are 3 groups, each with ${perGroup} passengers`);
+//   }, wait * 1000);
+//   console.log(`Will start boarding in ${wait} seconds.`);
+// };
+
+// const perGroup = 1000; // closure takes priority over global scope
+// boardPassenger(180, 3);
+
+// Coding Challenge #2
+// This is more of a thinking challenge than a coding challenge �
+// Your tasks:
+// 1. Take the IIFE below and at the end of the function, attach an event listener that
+// changes the color of the selected h1 element ('header') to blue, each time
+// the body element is clicked. Do not select the h1 element again!
+// 2. And now explain to yourself (or someone around you) why this worked! Take all
+// the time you need. Think about when exactly the callback function is executed,
+// and what that means for the variables involved in this example.
+
+(function () {
+  const header = document.querySelector("h1");
+  header.style.color = "red";
+  document.querySelector(`body`).addEventListener(`click`, function () {
+    header.style.color = `blue`;
+  });
+})();
+
+// the closure of the header variable still existed even after the iffe executed, so when we activate the call back function it still is able to access
+// the header variable
+
+// by the time the callback is executed, the iffe has been long gone as well as the variable
+//
