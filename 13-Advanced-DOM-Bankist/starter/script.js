9870792c5c3328a513eed5341fb10c7cd9697931
dflo55 <dflores2013@yahoo.com>
2023-02-23 03:11:18 -0600

    project finished

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -559,3 +559,18 @@ slider();
 // });
 //
 //
+
+// 202. LifeCycle DOM Events
+document.addEventListener(`DOMContentLoaded`, function (e) {
+  console.log(`HTML parsed and DOM ree built`);
+});
+
+window.addEventListener(`load`, function (e) {
+  console.log(`Page fully loaded`, e);
+});
+
+// window.addEventListener(`beforeunload`, function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = ``;
+// });
