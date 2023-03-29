036c363d9bf73513fad5f5028b058cdcb8723857
dflo55 <dflores2013@yahoo.com>
2023-03-29 03:50:35 -0500

    Form complete, refactoring Project Architecture

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -1,12 +1,80 @@
-'use strict';
+"use strict";
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+let map, mapEvent;
+
+// 232. Using the Geolocation API
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(
+        `https://www.google.com/maps/search/google+maps/@${latitude},${longitude}`
+      );
+      // 233. Displaying a Map Using Leaflet Library
+      const coords = [latitude, longitude];
+
+      map = L.map("map").setView(coords, 13);
+
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      // 234. Displaying a Map Marker
+      // Handling clicks on Map
+      map.on(`click`, function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove(`hidden`);
+        inputDistance.focus();
+      });
+    },
+    function () {
+      alert(`Could not get your position`);
+    }
+  );
+}
+//
+// 235. Rendering Workout Input Form
+form.addEventListener(`submit`, function (e) {
+  e.preventDefault();
+
+  // Clear Input Fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+      inputElevation.value;
+
+  // Display Marker
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: `running-popup`,
+      })
+    )
+    .setPopupContent(`Workout`)
+    .openPopup();
+});
+
+inputType.addEventListener(`change`, function () {
+  inputElevation.closest(`.form__row`).classList.toggle(`form__row--hidden`);
+  inputCadence.closest(`.form__row`).classList.toggle(`form__row--hidden`);
+});
